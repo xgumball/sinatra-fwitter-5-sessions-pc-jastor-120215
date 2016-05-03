723d859f816cd67ca49b9f255ef99f23059a216d
xgumball <anthonyviana4241997@gmail.com>
2016-05-03 22:23:24 +0000

    Fwitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
   get '/' do
@@ -13,18 +15,24 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  
   get '/tweet' do
     erb :tweet
+      if session[:user_id] #returns nil if there isn't one
+          erb :tweet
+      else
+        redirect "/login"
+      end
   end
 
   post '/tweet' do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(:id => session[:user_id])
     tweet = Tweet.new(:user => user, :status => params[:status])
     tweet.save
     redirect '/'
   end
 
-  get '/users' do
+  get '/signup' do
     @users = User.all
     erb :users
   end
@@ -34,4 +42,35 @@ class ApplicationController < Sinatra::Base
     @user.save
     redirect '/'
   end
+  
+  get '/login' do
+    erb :login
+  end
+ 
+  post '/login' do
+     user = User.find_by(:username => params[:username])
+     if user
+        session[:user_id] = user.id
+        redirect "/"
+     else
+        redirect "/signup"
+     end
+  end
+  
+  get '/logout' do
+    session.destroy
+    redirect '/login'
+  end
+  
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+ 
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
+  
 end
